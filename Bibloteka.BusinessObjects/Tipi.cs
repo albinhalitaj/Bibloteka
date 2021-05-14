29e9360d7b63ba2cad6147752037091410077e02
Albin Halitaj <albinhalitaj2019@hotmail.com>
2021-05-14 22:20:14 +0200

    added roli,tipi businessobjects

---
+++ Bibloteka.BusinessObjects/Tipi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bibloteka.BusinessObjects
+{
+    public class Tipi
+    {
+        public int TipiId { get; set; }
+
+        public string Emri { get; set; }
+
+        public string Pershkrimi { get; set; }
+
+        public int? InsertBy { get; set; }
+
+        public DateTime? InsertDate { get; set; }
+
+        public int? Lub { get; set; }
+
+        public int? Lun { get; set; }
+
+        public DateTime? Lud { get; set; }
+    }
+}
