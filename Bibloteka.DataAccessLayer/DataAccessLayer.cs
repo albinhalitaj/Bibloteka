5958e86e176e16af3e12bbade1e9befe5bdf3e31
Albin Halitaj <albinhalitaj2019@hotmail.com>
2021-05-15 17:39:03 +0200

    added DataAccessLayer

---
+++ Bibloteka.DataAccessLayer/DataAccessLayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Bibloteka.DataAccessLayer
+{
+    public static class DataAccessLayer
+    {
+        public static string AppConnection() => ConfigurationManager.ConnectionStrings["albikCon"].ConnectionString;
+    }
+}
