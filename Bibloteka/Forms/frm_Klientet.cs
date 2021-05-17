941cd76c9859ccc2f8964fce6c10676db1628640
Fatlind Bytyqi <fatlind-bytyqi@outlook.com>
2021-05-17 17:17:00 +0200

    add desing Klientet

--- Bibloteka/Forms/frm_Klientet.cs
+++ Bibloteka/Forms/frm_Klientet.cs
@@ -16,5 +16,10 @@ namespace Bibloteka.Forms
         {
             InitializeComponent();
         }
+
+        private void guna2HtmlLabel9_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
